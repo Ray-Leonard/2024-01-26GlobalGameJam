fd646e65e97da0ff09e96bac27b5e0d792520ac9
TachyonAgnes <para.bola@qq.com>
2024-01-27 16:01:40 -0500

    Add player

---
+++ RagdollBattle/Assets/Grab.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    private bool hold;
+    public KeyCode mousebutton;
+
+    void Update()
+    {
+        if(Input.GetKey(mousebutton)){
+            hold = true;
+        }else{
+            hold = false;
+            Destroy(GetComponent<FixedJoint2D>());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(hold){
+            Rigidbody2D rb = collision.transform.GetComponent<Rigidbody2D>();
+            if(rb != null){
+                FixedJoint2D fj = transform.gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+                fj.connectedBody = rb;
+            }else{
+                FixedJoint2D fj = transform.gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+            }
+        }
+    }
+}
