b18a85c0d53002d0307c1599db678fbb781880f1
Tales Mariano <Tales.Mariano1@gmail.com>
2024-01-27 19:14:25 -0500

    test gamepad input

--- RagdollBattle/Assets/_Scripts/Arms.cs
+++ RagdollBattle/Assets/_Scripts/Arms.cs
@@ -9,14 +9,31 @@ public class Arms : MonoBehaviour
     public Camera cam;
     public KeyCode mousebutton;
 
+    public bool useController = false;
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerpos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
-        Vector3 difference = playerpos - transform.position;
-        float rotationZ = Mathf.Atan2(difference.x, -difference.y) * Mathf.Rad2Deg;
-        if(Input.GetKey(mousebutton)){
-            rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotationZ, speed * Time.fixedDeltaTime));
+        if (useController)
+        {
+            if (Mathf.Abs(  Input.GetAxisRaw("Horizontal")) > 0.1f || Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.1f ){
+                float rotationZ = Mathf.Atan2(Input.GetAxisRaw("Horizontal"), -Input.GetAxisRaw("Vertical")) * Mathf.Rad2Deg;
+                rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotationZ, speed * Time.fixedDeltaTime));
+            }
+
+
         }
+        else
+        {
+            Vector3 playerpos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            Vector3 difference = playerpos - transform.position;
+            float rotationZ = Mathf.Atan2(difference.x, -difference.y) * Mathf.Rad2Deg;
+            if (Input.GetKey(mousebutton))
+            {
+                rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotationZ, speed * Time.fixedDeltaTime));
+            }
+        }
+
+
     }
 }
