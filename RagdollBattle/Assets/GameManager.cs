4118c7fb89384eebe5039c7b1646dca05bb8132e
TachyonAgnes <para.bola@qq.com>
2024-01-28 10:56:12 -0500

    Add game manager

---
+++ RagdollBattle/Assets/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class GameManager : MonoBehaviour
+{
+    public List<PlayerController> controllerList = new List<PlayerController>();
+    private bool isGameHasWinner = false;
+    public float timer = 180;
+    private void Start()
+    {
+        // find all objects with player controller script
+        PlayerController[] controllers = GameObject.FindObjectsOfType<PlayerController>();
+        // add them to the list
+        for (int i = 0; i < controllers.Length; i++)
+        {
+            controllerList.Add(controllers[i]);
+        }
+    }
+
+    private void Update()
+    {
+        if(CheckGameEnd()){
+            foreach (PlayerController controller in controllerList)
+            {
+                if(controller.GetComponent<BodyPartController>().isLongLegs){
+                    Debug.Log("Winner is£º" + controller.PlayerID);
+                    isGameHasWinner = true; 
+                    break;
+                }
+            }
+            if(!isGameHasWinner){
+                Debug.Log("Draw!");
+            }
+        }
+    }
+
+    private bool CheckGameEnd(){
+        timer -= Time.deltaTime;
+        if(timer <= 0){
+            return true;
+        }
+        return false;
+    }
+}
