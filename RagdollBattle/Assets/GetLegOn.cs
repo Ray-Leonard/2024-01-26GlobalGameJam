b83774d9722d5ed6310c6328286af41a27975810
TachyonAgnes <para.bola@qq.com>
2024-01-27 22:18:00 -0500

    Update

---
+++ RagdollBattle/Assets/GetLegOn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetLegOn : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerWithLegs;
+    private PlayerController controller;
+
+    private void Awake()
+    {
+        controller = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+       {
+            if (collision.gameObject.tag == "LegPickUp")
+            {
+                GameObject playerWithLegs = Instantiate(PlayerWithLegs, transform.position, Quaternion.identity);
+                playerWithLegs.GetComponent<PlayerController>().playerID = controller.playerID;
+                Destroy(controller.gameObject);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
